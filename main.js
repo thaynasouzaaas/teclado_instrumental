ee02c7559ec66690faab8c905af35b59541b5f1d
Thayna Luiza Souza Silva <thayna.silva@oi.net.br>
2023-08-15 14:52:58 -0300

    versão final

---
+++ main.js
@@ -0,0 +1,39 @@
+function tocaSom (selectorAudio) {
+    const elemento = document.querySelector(selectorAudio);
+
+    
+    if (elemento != null && elemento.localName === 'audio' ) {
+            elemento.play();
+        
+    }
+   else {
+     //alert ('Elemento não encontrado')
+     console.log ('elemento não encontrado ou seletor inválido');
+   }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador =0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas [contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function () {
+        tocaSom (idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+
+        if (event.code === 'Space' || event.code === 'Enter'){
+        tecla.classList.add('ativa');
+    } 
+
+}
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+
+}
+
